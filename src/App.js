dacdb37bee7322945ecf909c543471fd8e85d8a7
Agimezelu Emmanuel <agimezeluemmanuel@gmail.com>
2023-06-10 10:36:53 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { useReducer } from 'react';
 
+const initialState = {
+  count: 0,
+  showText: false
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INC":
+      return {
+        count: state.count + 1,
+        showText: !state.showText
+      }
+    case "DEC":
+      return {
+        count: state.count - 1,
+        showText: !state.showText
+      }
+    default:
+      return state;
+  }
+};
 function App() {
+  const [state, dispatch] = useReducer(reducer,initialState)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{state.count}</h1>
+        <button onClick={()=>{ dispatch({type: "INC"})}}>Increase</button>
+        <button onClick={()=>{ dispatch({type: "DEC"})}}>Decrease</button>
+       {state.showText && <h1>This is a text</h1>}
       </header>
     </div>
   );
